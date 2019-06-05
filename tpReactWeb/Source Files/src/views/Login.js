2c28ef66a70970a2eb6841ce3f1e456cde9c72a4
alejandro <a>
2019-06-05 17:54:37 -0300

    vengo borrando bien

--- tpReactWeb/Source Files/src/views/Login.js
+++ tpReactWeb/Source Files/src/views/Login.js
@@ -1,9 +1,5 @@
 import React, {Component} from "react";
-import { Container, Row, Col, Card, CardHeader, CardBody } from "shards-react";
-
-import PageTitle from "../components/common/PageTitle";
-import RestClient from "../rest_api/RestClient";
-import { Button } from "shards-react";
+import { Container } from "shards-react";
 import LoginInterno from "../components/Login/LoginInterno";
 
 class Login extends Component {
@@ -16,39 +12,18 @@ class Login extends Component {
     }
   }
 
-  componentWillMount() {
-    this.fetchProductos();
-  }
-
-  fetchProductos() {
-    RestClient.getProductos().then(json => {
-      this.setState({productos: json, isLoaded: true})
-    });
-  }
-
-  handlerEliminarClickItem(numeroProducto) {
-    RestClient.bajaProducto(numeroProducto).then(data => this.fetchProductos());
-  }
-  handlerNuevoProductoClick() {
-    this.props.history.push('/nuevo-producto')
-  }
-
   render() {
-    var {isLoaded, productos} = this.state;
-    if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <Container fluid className="main-content-container px-4 pb-4">
-          <div className="error">
-            <div className="error__content">
-              <h3>Bienvenido al sistema de pedidos</h3>
-            <LoginInterno/>
-            </div>
+    return (
+      <Container fluid className="main-content-container px-4 pb-4">
+        <div className="error">
+          <div className="error__content">
+            <h3>Bienvenido al sistema de pedidos</h3>
+          <LoginInterno/>
           </div>
-        </Container>
-      );
-    }
+        </div>
+      </Container>
+    );
+
   }
 }
 
